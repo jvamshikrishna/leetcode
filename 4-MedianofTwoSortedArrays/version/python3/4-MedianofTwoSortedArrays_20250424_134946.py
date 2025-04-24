42e1e53e82ba9821b10a31d69327db7b1d1c1b20
Vamshi Krishna <37012074+jvamshikrishna@users.noreply.github.com>
2025-04-24 13:49:46 -0400

    Create file

---
+++ 4-MedianofTwoSortedArrays/version/python3/4-MedianofTwoSortedArrays_20250424_134946.py
@@ -0,0 +1,23 @@
+# Last updated: 4/24/2025, 1:49:46 PM
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+        merged = []
+        i = j = 0
+
+        while i< len(nums1) and j<len(nums2):
+            if nums1[i]<nums2[j]:
+                merged.append(nums1[i])
+                i+=1
+            else:
+                merged.append(nums2[j])
+                j+=1
+
+        merged.extend(nums1[i:])
+        merged.extend(nums2[j:])
+
+        n = len(merged)
+        if n%2 == 1:
+            return float(merged[n//2])
+        else:
+            return float(merged[n//2 -1]+merged[n//2]) /2
