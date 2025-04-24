9970b14e9495b518137d694889d67c771d01f343
Vamshi Krishna <37012074+jvamshikrishna@users.noreply.github.com>
2025-04-24 19:06:13 -0400

    Create file

---
+++ 8-StringtoInteger(atoi)/version/python3/8-StringtoInteger(atoi)_20250424_190613.py
@@ -0,0 +1,47 @@
+# Last updated: 4/24/2025, 7:06:13 PM
+'''
+its actually easy just understand the flow of questions and follow the steps 
+1. remove the white space
+2. assign the sign
+3. convert digit to int
+4. check if in 32 bit range
+'''
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+
+        i = 0
+        n = len(s)
+        INT_MAX = 2**31 - 1
+        INT_MIN = -2**31
+
+        # step 1: skip the whietspaces
+        while i< n and s[i] == ' ':
+            i+=1
+        
+        #step 2: handle the sign
+        sign = 1
+        if i<n and (s[i]=='+' or s[i]=='-'):
+            if s[i]== '-':
+                sign = -1
+            i+=1
+        
+        #step 3: convert digit to integer
+        result = 0
+        while i<n and s[i].isdigit():
+            result = result*10 + int(s[i])
+            i+=1
+
+        result = result * sign
+
+        #step 4: check if its in range of 32 bit
+        if result < INT_MIN:
+            return INT_MIN
+        if result > INT_MAX:
+            return INT_MAX
+        return result
+
+
+
+
+        
