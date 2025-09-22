899fc5856ef596db09ac2dfd6aca584ffa21194b
Vamshi Krishna <37012074+jvamshikrishna@users.noreply.github.com>
2025-09-21 22:33:12 -0400

    Create 13-RomantoInteger

---
+++ 13-RomantoInteger/version/python/13-RomantoInteger_20250921_223312.py
@@ -0,0 +1,23 @@
+# Last updated: 9/21/2025, 10:33:12 PM
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map ={
+            'I':1,
+            'V':5,
+            'X':10,
+            'L':50,
+            'C':100,
+            'D':500,
+            'M':1000
+        }
+        result = 0
+
+        for i in range(len(s)):
+            current_value = roman_map[s[i]]
+
+            if i< len(s)-1 and current_value < roman_map[s[i+1]]:
+                result -= current_value
+            else:
+                result += current_value
+
+        return result
